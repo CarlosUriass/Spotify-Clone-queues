37600ac8821dd8d813d4450793ab83f2fd6f16ac
Carlos Urias <hicarlosurias@gmail.com>
2024-09-01 11:28:34 -0700

    Typescript logic for fetch the data in JSON, song added in the song reproduction to click buttons

--- main.js
+++ main.js
@@ -8,13 +8,6 @@ var Cancion = /** @class */ (function () {
     }
     return Cancion;
 }());
-function minSeg(duracion) {
-    var minutos = Math.floor(duracion / 60);
-    var segundos = duracion % 60;
-    // Si los segundos son menores a 10, agregamos un 0 al inicio
-    var segundosStr = segundos < 10 ? "0" + segundos.toString() : segundos.toString();
-    return "".concat(minutos, ":").concat(segundosStr);
-}
 fetch('/playlist.json') // Cargar datos desde un archivo JSON
     .then(function (response) {
     if (!response.ok) {
@@ -36,4 +29,25 @@ fetch('/playlist.json') // Cargar datos desde un archivo JSON
 })
     .catch(function (error) {
     console.error('Error:', error);
+});
+function minSeg(duracion) {
+    var minutos = Math.floor(duracion / 60);
+    var segundos = duracion % 60;
+    // Si los segundos son menores a 10, agregamos un 0 al inicio
+    var segundosStr = segundos < 10 ? "0" + segundos.toString() : segundos.toString();
+    return "".concat(minutos, ":").concat(segundosStr);
+}
+// Agregar cancion en el HTML
+function agregarCancion(cancion) {
+    var musicaPlay = document.querySelector('#musicaPlay');
+    if (!musicaPlay.querySelector('#CancionPlay')) {
+        var cancionHTML = "\n            <div id=\"CancionPlay\" class=\"d-flex align-items-center justify-content-left\">\n                <img src=".concat(cancion.portada, " alt=\"\" style=\"height: 70px;\">\n                <div class=\"ms-3\">\n                    <div id=\"songTitle\" class=\"fw-semibold text-white\">").concat(cancion.titulo, "</div>\n                    <div id=\"artistName\" class=\"text-white\" style=\"font-size: 0.9em; line-height: 15px; opacity: 0.8;\">\n                        ").concat(cancion.artista, "\n                    </div>\n                </div>\n            </div>\n        ");
+        musicaPlay.insertAdjacentHTML('afterbegin', cancionHTML);
+    }
+}
+var buttons = document.querySelectorAll('.btn');
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        agregarCancion(playlist[2]);
+    });
 });
--- main.ts
+++ main.ts
@@ -14,19 +14,6 @@ class Cancion {
     }
 }
 
-
-function minSeg(duracion: number): string {
-    const minutos: number = Math.floor(duracion / 60);
-    let segundos: number = duracion % 60;
-
-    // Si los segundos son menores a 10, agregamos un 0 al inicio
-    let segundosStr: string = segundos < 10 ? "0" + segundos.toString() : segundos.toString();
-
-    return `${minutos}:${segundosStr}`;
-}
-
-
-
 fetch('/playlist.json') // Cargar datos desde un archivo JSON
     .then(response => {
         if (!response.ok) {
@@ -51,3 +38,53 @@ fetch('/playlist.json') // Cargar datos desde un archivo JSON
     .catch(error => {
         console.error('Error:', error);
     });
+
+
+function minSeg(duracion: number): string {
+    const minutos: number = Math.floor(duracion / 60);
+    let segundos: number = duracion % 60;
+
+    // Si los segundos son menores a 10, agregamos un 0 al inicio
+    let segundosStr: string = segundos < 10 ? "0" + segundos.toString() : segundos.toString();
+
+    return `${minutos}:${segundosStr}`;
+}
+
+
+// Agregar cancion en el HTML
+function agregarCancion(cancion: Cancion) {
+    const musicaPlay = document.querySelector('#musicaPlay') as HTMLElement;
+
+    if (!musicaPlay.querySelector('#CancionPlay')) {
+        const cancionHTML = `
+            <div id="CancionPlay" class="d-flex align-items-center justify-content-left">
+                <img src=${cancion.portada} alt="" style="height: 70px;">
+                <div class="ms-3">
+                    <div id="songTitle" class="fw-semibold text-white">${cancion.titulo}</div>
+                    <div id="artistName" class="text-white" style="font-size: 0.9em; line-height: 15px; opacity: 0.8;">
+                        ${cancion.artista}
+                    </div>
+                </div>
+            </div>
+        `;
+
+        musicaPlay.insertAdjacentHTML('afterbegin', cancionHTML);
+    }
+
+}
+
+const buttons = document.querySelectorAll('.btn');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        agregarCancion(playlist[2])
+    });
+});
+
+
+
+
+
+
